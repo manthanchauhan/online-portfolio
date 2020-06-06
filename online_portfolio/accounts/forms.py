dcd5bf3d601b4e5a9896ddf77c9627f5ece0f096
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-06-06 12:49:45 +0530

    Enter Email view complete with styling.

--- online_portfolio/accounts/forms.py
+++ online_portfolio/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
 def existing_email(email):
     """
     It is a validator which checks if the any user with the given email exists in the system.
@@ -15,7 +16,15 @@ def existing_email(email):
         return
 
     # user exists
-    raise forms.ValidationError('User with this email already exists. Please login.')
+    raise forms.ValidationError("User with this email already exists. Please login.")
+
 
 class EnterEmailForm(forms.Form):
-    user_email = forms.EmailField(label='Enter Email Here', max_length=100, validators=[existing_email])
+    user_email = forms.EmailField(
+        label="Enter Email Here",
+        max_length=100,
+        validators=[existing_email],
+        widget=forms.EmailInput(
+            attrs={"placeholder": "Enter Your Email", "class": "form-control"}
+        ),
+    )
