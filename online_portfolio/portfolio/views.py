bd508d3190833d75c76316584e19c98c0f212948
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-06-10 20:03:20 +0530

    added login required to portfolio edit page.

--- online_portfolio/portfolio/views.py
+++ online_portfolio/portfolio/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class PortfolioEdit(View):
+class PortfolioEdit(LoginRequiredMixin, View):
     template = "portfolio/portfolio_edit.html"
 
     def get(self, request):
