be0ce49d285cec970bdf8118edcd8175afeff1e1
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-06-11 22:14:29 +0530

    using gmail for emails

--- online_portfolio/online_portfolio/settings.py
+++ online_portfolio/online_portfolio/settings.py
@@ -127,10 +127,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 # Email settings
-SENDGRID_API_KEY = config("SENDGRID_KEY")
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "pythonic913@gmail.com"
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
