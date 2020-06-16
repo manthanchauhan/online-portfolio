63aabf4d1c1d324a534327c4b277d143b900fc52
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-06-16 16:18:34 +0530

    reduced main name size.

---
+++ online_portfolio/static/portfolio/js/edit.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".project_image").on("click", function() {
+    // var modal = document.getElementById("myModal");
+    // modal.style.display = "block";
+    console.log("hi");
+    $("#myModal").modal("show");
+  });
+});
