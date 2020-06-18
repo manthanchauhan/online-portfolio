1b94a9dc8d1963ef33ef874e3e4ba2aab41a0e01
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-06-18 15:37:43 +0530

    reverted back to local static files for development, will switch to s3 in prod.

--- online_portfolio/online_portfolio/settings.py
+++ online_portfolio/online_portfolio/settings.py
@@ -128,26 +128,30 @@ LOGIN_REDIRECT_URL = "/portfolio/edit/"
 LOGOUT_REDIRECT_URL = "/accounts/login"
 
 # static files (Amazon S3) configuration
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "online-portfolio123"
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com"
-
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400",
-}
-
-AWS_LOCATION = "static"
+# AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY")
+# AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME = "online-portfolio123"
+# AWS_DEFAULT_ACL = None
+# AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com"
+#
+# AWS_S3_OBJECT_PARAMETERS = {
+#     "CacheControl": "max-age=86400",
+# }
+#
+# AWS_LOCATION = "static"
+#
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# STATIC_URL = "https://" + AWS_S3_CUSTOM_DOMAIN + "/" + AWS_LOCATION + "/"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # this is from where, django wills serve files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = "https://" + AWS_S3_CUSTOM_DOMAIN + "/" + AWS_LOCATION + "/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATIC_URL = "/static/"
+
 
 # phone number setup
 PHONENUMBER_DB_FORMAT = "INTERNATIONAL"
