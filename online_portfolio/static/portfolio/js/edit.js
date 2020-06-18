f156feac69679790dc7644ee0336743948c857b0
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-06-18 13:06:10 +0530

    finalized an early version of portfolio, to start working on backend, a bit.

--- online_portfolio/static/portfolio/js/edit.js
+++ online_portfolio/static/portfolio/js/edit.js
@@ -1,8 +1,5 @@
 $(function () {
   $(".project_image").on("click", function() {
-    // var modal = document.getElementById("myModal");
-    // modal.style.display = "block";
-    console.log("hi");
     $("#myModal").modal("show");
   });
 });
